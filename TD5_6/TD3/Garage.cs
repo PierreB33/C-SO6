f6758734da1fdfb801363ee8f469a6fd3cb544ce
PierreB33 <119490594+PierreB33@users.noreply.github.com>
2024-03-12 12:21:50 +0100

    Add files via upload

---
+++ TD5_6/TD3/Garage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD5_6
+{
+    internal class Garage : IMoyenne<List<Voiture>>
+    {
+        private string num_siret;
+        private List<Voiture> flotte;
+
+        public Garage(string num_siret, List<Voiture> flotte)
+        {
+            this.num_siret = num_siret;
+            this.flotte = flotte;
+        }
+        public double Moyenne(List<Voiture> voitures)
+        { 
+            double somme = 0;
+            double moyenne = 0;
+            if (voitures.Count()==0)
+            {
+                return 0;
+            }
+            else
+            {
+                foreach(var voiture in voitures)
+                {
+                    somme += voiture.Prix_achat;
+                }
+                moyenne = somme / voitures.Count();
+                return moyenne;
+            }
+
+
+        }
+    }
+}
---
+++ TD5_6/TD3/Voiture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TD3;
+
+namespace TD5_6
+{
+    internal class Voiture
+    {
+        private string plaque_immatriculation;
+        private string marque;
+        private string modele;
+        private int prix_achat;
+
+        public Voiture(string plaque_immatriculation,string marque,string modele,int prix_achat)
+        {
+            this.plaque_immatriculation = plaque_immatriculation;
+            this.marque = marque;
+            this.modele = modele;
+            this.prix_achat = prix_achat;
+        }
+        public int Prix_achat
+        {
+            get { return prix_achat; }
+            set { prix_achat = value; }
+        }
+            
+    }
+}
